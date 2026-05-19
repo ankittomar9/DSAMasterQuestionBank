33b972dca2272311463896edfd47667bc177bff3
ankittomar9 <tomarankit78@gmail.com>
2026-05-19 09:42:50 +0530

    New Added Programs Revision

---
+++ LeetCode150/LeetCode150_1/Q_15_Candy.java
@@ -0,0 +1,10 @@
+package LeetCode150.LeetCode150_1;
+
+public class Q_15_Candy {
+    public static void main(String[] args) {
+        
+    }
+    public static long candy(int[] ratings){
+
+    }
+}
