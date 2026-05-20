25260ba5c05c74d035a0fb3551feedb205d2ce2c
ankittomar9 <tomarankit78@gmail.com>
2026-05-20 18:34:59 +0530

    New Added Programs Revision

---
+++ LeetCode150/LeetCode150_1/Q_20_Longest_Common_Prefix.java
@@ -0,0 +1,29 @@
+package LeetCode150.LeetCode150_1;
+
+public class Q_20_Longest_Common_Prefix {
+    public static void main(String[] args) {
+        String[] strs1 = {"flower", "flow", "flight"};
+        String[] strs2 = {"dog", "racecar", "car"};
+
+        System.out.println("Input  : " + java.util.Arrays.toString(strs1));
+        System.out.println("Output : " + longestCommonPrefix(strs1));
+
+        System.out.println("\\nInput  : " + java.util.Arrays.toString(strs2));
+        System.out.println("Output : \\"" + longestCommonPrefix(strs2) + "\\"");
+    }
+
+    public static String longestCommonPrefix(String[] strs){
+        if(strs==null || strs.length==0){return "";}
+
+        String prefix=strs[0];
+
+        for(int i=1;i<strs.length;i++){
+            
+            while(strs[i].indexOf(prefix)!=0){
+                if(prefix.isEmpty()){return " "; }
+            }
+            prefix=prefix.substring(0,prefix.length()-1);
+        }
+        return prefix;
+    }
+}
