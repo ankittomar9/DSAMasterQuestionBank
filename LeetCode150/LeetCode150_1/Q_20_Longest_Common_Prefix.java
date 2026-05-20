c63c6eb66cd95adb30f89cfeacdaf7164a4c9785
ankittomar9 <tomarankit78@gmail.com>
2026-05-20 18:46:44 +0530

    New Added Programs Revision

--- LeetCode150/LeetCode150_1/Q_20_Longest_Common_Prefix.java
+++ LeetCode150/LeetCode150_1/Q_20_Longest_Common_Prefix.java
@@ -1,15 +1,17 @@
 package LeetCode150.LeetCode150_1;
 
+import java.util.Arrays;
+
 public class Q_20_Longest_Common_Prefix {
     public static void main(String[] args) {
         String[] strs1 = {"flower", "flow", "flight"};
         String[] strs2 = {"dog", "racecar", "car"};
 
-        System.out.println("Input  : " + java.util.Arrays.toString(strs1));
-        System.out.println("Output : " + longestCommonPrefix(strs1));
+        System.out.println("Input  \n: " +Arrays.toString(strs1));
+        System.out.println("Output \n: " + longestCommonPrefix(strs1));
 
-        System.out.println("\\nInput  : " + java.util.Arrays.toString(strs2));
-        System.out.println("Output : \\"" + longestCommonPrefix(strs2) + "\\"");
+        System.out.println("\nInput  : " +Arrays.toString(strs2));
+        System.out.println("Output : \n"  + longestCommonPrefix(strs2) );
     }
 
     public static String longestCommonPrefix(String[] strs){
@@ -20,9 +22,12 @@ public class Q_20_Longest_Common_Prefix {
         for(int i=1;i<strs.length;i++){
             
             while(strs[i].indexOf(prefix)!=0){
-                if(prefix.isEmpty()){return " "; }
+                if(prefix.isEmpty()){
+                    return "";
+                 }
+                  prefix=prefix.substring(0,prefix.length()-1);
             }
-            prefix=prefix.substring(0,prefix.length()-1);
+           
         }
         return prefix;
     }
