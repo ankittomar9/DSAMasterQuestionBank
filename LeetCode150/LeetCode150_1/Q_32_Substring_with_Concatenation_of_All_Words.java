e4cf35cb42843068c8f34be7f081aa346fbee13e
ankittomar9 <tomarankit78@gmail.com>
2026-05-26 17:04:58 +0530

    New Added Programs Revision

--- LeetCode150/LeetCode150_1/Q_32_Substring_with_Concatenation_of_All_Words.java
+++ LeetCode150/LeetCode150_1/Q_32_Substring_with_Concatenation_of_All_Words.java
@@ -1,11 +1,16 @@
 package LeetCode150.LeetCode150_1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Q_32_Substring_with_Concatenation_of_All_Words {
     public static void main(String[] args) {
-        
+          String s = "barfoothefoobarman"; System.out.println("Input s     : " + s);
+        String[] words = {"foo", "bar"};  System.out.println("Input words : " + Arrays.toString(words));
+        System.out.println("Output      : " + findSubstring(s, words));
     }
 
     public static List<Integer> findSubstring(String s,String[] words){
@@ -13,8 +18,8 @@ public class Q_32_Substring_with_Concatenation_of_All_Words {
         if(s==null || words ==null || words.length==0){return result;}
         
         int wordLen=words[0].length(); int totalLength=wordLen*words.length;
-        Map<Integer,Integer> map=new HashMap<>();
-
+        
+        Map<String,Integer> map=new HashMap<>();
         for(int i=0;i<words.length;i++){
             String word=words[i];
             if(map.containsKey(word)){
@@ -24,8 +29,33 @@ public class Q_32_Substring_with_Concatenation_of_All_Words {
             }
         }
 
-        
+        for(int i=0;i<=s.length()-totalLength;i++){
+            Map<String,Integer> windowCount=new HashMap<>();
+            boolean valid=true;
 
+        for(int j=0;j<words.length;j++){
+            int start=i+j * wordLen;
+            int end =start +wordLen;
+            String word =s.substring(start,end);
+            
+             if(!map.containsKey(word)){
+                valid=false; break;
+             }   
+
+             if(windowCount.containsKey(word)){
+                windowCount.put(word, windowCount.get(word)+1);
+             }else{
+                windowCount.put(word, 1);
+             }
+             if(windowCount.get(word)>map.get(word)){
+                valid=false; break;
+             }
+         }
+         if(valid && windowCount.equals(map)){
+            result.add(i);
+         }
+        }
+        return result;
     }
 }
 
