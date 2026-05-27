00e7ca7d50d154e321482f111e03da50d89d3770
ankittomar9 <tomarankit78@gmail.com>
2026-05-27 12:01:30 +0530

    New Added Programs Revision

--- LeetCode150/LeetCode150_1/Q_37_Set_Matrix_Zeroes.java
+++ LeetCode150/LeetCode150_1/Q_37_Set_Matrix_Zeroes.java
@@ -1,5 +1,53 @@
 package LeetCode150.LeetCode150_1;
 
 public class Q_37_Set_Matrix_Zeroes {
-    
+    public static void main(String[] args) {
+         int[][] matrix={{1,1,1},{1,0,1},{1,1,1}};
+           System.out.println("Input Matrix:");        printMatrix(matrix);
+        setZeroes(matrix);
+        System.out.println("\nAfter Setting Zeroes:");
+        printMatrix(matrix);
+    }
+    public static void setZeroes(int[][] matrix){
+        if(matrix==null || matrix.length==0){return;}
+        int m=matrix.length; int n=matrix[0].length;
+        boolean firstRowZero=false; boolean firstColumnZero=false;
+
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0){ firstRowZero=true;}
+        }   
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0){ firstColumnZero=true;}
+        }   
+        for(int i=1;i<m;i++){
+            for(int j=1; j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1; j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+            if(firstRowZero){
+                for(int j=0;j<n;j++){ matrix[0][j]=0;}
+            }
+            if(firstColumnZero){
+                for(int i=0;i<m;i++){ matrix[i][0]=0;}
+            }
+            
+    }       
+     public static void printMatrix(int[][] matrix){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
 }
