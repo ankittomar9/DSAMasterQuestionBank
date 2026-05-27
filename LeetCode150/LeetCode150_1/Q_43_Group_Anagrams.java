cb5c77eccf93f66921510153c679d67b4a79cf64
ankittomar9 <tomarankit78@gmail.com>
2026-05-27 16:29:23 +0530

    New Added Programs Revision

---
+++ LeetCode150/LeetCode150_1/Q_43_Group_Anagrams.java
@@ -0,0 +1,13 @@
+package LeetCode150.LeetCode150_1;
+
+import java.util.HashMap;
+
+public class Q_43_Group_Anagrams {
+    public static void main(String[] args) {
+        
+    }
+    public static List<List<String>> groupAnagrams(String[] strs){
+        if(strs==null || strs.length==0){ return new ArrayList<>();}
+        Map<String ,List<String>>map=new HashMap<>();
+    }
+}
