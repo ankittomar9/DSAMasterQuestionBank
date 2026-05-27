c7b5a3e67304af1c8a4e3627ed291ce4fafb0ccc
ankittomar9 <tomarankit78@gmail.com>
2026-05-27 12:12:12 +0530

    New Added Programs Revision

--- LeetCode150/LeetCode150_1/Q_40_Isomorphic_Strings.java
+++ LeetCode150/LeetCode150_1/Q_40_Isomorphic_Strings.java
@@ -1,5 +1,26 @@
 package LeetCode150.LeetCode150_1;
 
 public class Q_40_Isomorphic_Strings {
-    
+    public static void main(String[] args) {
+        
+    }
+    public static boolean isIsomorphic(String s ,String t){
+        if(s==null || t==null ||s.length() !=t.length()){
+            return false;
+        }
+        int[] mapS=new int[256];
+        int[] mapT=new int[256];
+
+        for(int i=0;i<s.length();i++){
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
+
+            if(mapS[c1]!=mapT[c2]){
+                return false;
+            }
+            mapS[c1]=i+1;
+            mapT[c2]=i+1;
+        }
+        return true;
+    }
 }
