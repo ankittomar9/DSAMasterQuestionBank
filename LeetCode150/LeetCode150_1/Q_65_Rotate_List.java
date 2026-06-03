8fe0d0ae599032ed1a58622d74f4d50fe5a63f4a
tomar <tomarankit78@gmail.com>
2026-06-03 18:38:08 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_65_Rotate_List.java
+++ LeetCode150/LeetCode150_1/Q_65_Rotate_List.java
@@ -1,5 +1,51 @@
 package LeetCode150.LeetCode150_1;
-
 public class Q_65_Rotate_List {
-    
+    public static void main(String[] args) {
+        ListNode head1=new ListNode(1,new ListNode(2,new ListNode(3,
+            new ListNode(4,new ListNode(5)))));
+
+                    ListNode head2=new ListNode(0,new ListNode(1, new ListNode(2)));
+            System.out.println(" Input List : "+listToString(head1));
+             System.out.println(" Output    : "+listToString(rotateRight(head1,2)));
+
+             System.out.println("\nInput List : "+listToString(head2));
+             System.out.println(" Output    : "+listToString(rotateRight(head2,4)));
+    }
+    public static ListNode rotateRight(ListNode head,int k){
+        if(head==null || head.next==null || k==0){ return head;        }
+
+        int length=1;        ListNode tail=head;
+            while(tail.next!=null){
+                tail=tail.next;
+                length++;
+            }
+            tail.next=head;
+            k=k%length;
+            if(k==0){
+                tail.next=null;
+                return head;
+            }
+
+            ListNode newTail= head;
+            for(int i=0;i<length-k-1;i++){
+                newTail=newTail.next;
+            }
+            ListNode newHead=newTail.next;
+            newTail.next=null;
+            return newHead;
+    }
+    public static String listToString(ListNode head){
+        StringBuilder sb=new StringBuilder();
+        sb.append("["); ListNode temp=head;
+        while(temp!=null){
+            sb.append(temp.val);
+            if(temp.next!=null){ sb.append(", ");}
+            temp=temp.next;
+        }
+        sb.append("]");return sb.toString();
+    }
+    public static class ListNode{
+        int val;ListNode next;public ListNode(){};public ListNode(int val){this.val=val;}
+        public ListNode(int val,ListNode next){this.val=val;this.next=next;}
+    }
 }
