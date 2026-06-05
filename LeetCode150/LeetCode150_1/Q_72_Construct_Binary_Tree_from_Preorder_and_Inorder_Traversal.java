5cfc203803ff03dc8ed251cff52ccd8cb2a2eb3d
tomar <tomarankit78@gmail.com>
2026-06-05 19:16:28 +0530

    New Added Program DSA

---
+++ LeetCode150/LeetCode150_1/Q_72_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -0,0 +1,8 @@
+package LeetCode150.LeetCode150_1;
+
+public class Q_72_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal {
+    public static void main(String[] args) {
+        
+    }
+    
+}
