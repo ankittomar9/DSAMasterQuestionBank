b395bd5670445c122e703329d5bfe1daf6456095
tomar <tomarankit78@gmail.com>
2026-06-05 15:43:52 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_69_Same_Tree.java
+++ LeetCode150/LeetCode150_1/Q_69_Same_Tree.java
@@ -2,14 +2,28 @@ package LeetCode150.LeetCode150_1;
 
 public class Q_69_Same_Tree {
     public static void main(String[] args) {
+         TreeNode p1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode q1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+
+        System.out.println("Input p : [1, 2, 3]"); System.out.println("\nInput q : [1, 2, 3]");
+        System.out.println("Output  : " + isSameTree(p1, q1));
+
+        // Test case 2: different structure
+        TreeNode p2 = new TreeNode(1, new TreeNode(2), null);
+        TreeNode q2 = new TreeNode(1, null, new TreeNode(2));
+
+        System.out.println("\nInput p : [1, 2]");
+        System.out.println("Input q : [1, null, 2]");
+        System.out.println("Output  : " + isSameTree(p2, q2));
+    }
+    public static boolean isSameTree(TreeNode p,TreeNode q){
+        if(p==null && q==null ){            return true;        }
         
-    }
-    public static int maxDepth(TreeNode root){
-        if(root==null){            return 0;        }
-        int leftHeight=maxDepth(root.left);
-           int rightHeight=maxDepth(root.right);
-           return leftHeight+rightHeight+1;
-    }
+        if(p==null ||q==null  || p.val!=q.val){return false;}
+        
+        boolean result =isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return result;
+    }   
 
     public static class TreeNode{
         int val;TreeNode left;TreeNode right;
