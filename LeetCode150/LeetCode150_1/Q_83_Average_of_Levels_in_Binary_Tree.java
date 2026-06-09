11b6482d123abe2e95a6332c783dc7f782bfff1b
tomar <tomarankit78@gmail.com>
2026-06-09 13:06:27 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_83_Average_of_Levels_in_Binary_Tree.java
+++ LeetCode150/LeetCode150_1/Q_83_Average_of_Levels_in_Binary_Tree.java
@@ -1,5 +1,13 @@
 package LeetCode150.LeetCode150_1;
 
 public class Q_83_Average_of_Levels_in_Binary_Tree {
-    
+    public static void main(String[] args) {
+         // Build tree: [3,9,20,null,null,15,7]
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+
+        System.out.println("Input Tree : [3, 9, 20, null, null, 15, 7]");
+        System.out.println("Average of Levels: " + averageOfLevels(root));
+    }
 }
