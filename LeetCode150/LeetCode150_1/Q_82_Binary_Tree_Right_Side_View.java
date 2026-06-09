0bfaa08405eaef7bd73c1e3fc4fbf1a557853784
tomar <tomarankit78@gmail.com>
2026-06-09 12:54:15 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_82_Binary_Tree_Right_Side_View.java
+++ LeetCode150/LeetCode150_1/Q_82_Binary_Tree_Right_Side_View.java
@@ -1,5 +1,14 @@
 package LeetCode150.LeetCode150_1;
 
 public class Q_82_Binary_Tree_Right_Side_View {
-    
+    public static void main(String[] args) {
+        
+    }
+     public static class TreeNode{
+        int val;TreeNode left;TreeNode right;
+        TreeNode (){};
+        TreeNode (int val){this.val=val;}
+        TreeNode(int val,TreeNode left,TreeNode right){
+            this.val=val;this.left=left;this.right=right;        }
+    }
 }
