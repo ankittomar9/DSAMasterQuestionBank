904847fe4f791afde21c7160247f576cd197abaa
tomar <tomarankit78@gmail.com>
2026-06-10 11:54:52 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_86_Minimum_Absolute_Difference_in_BST.java
+++ LeetCode150/LeetCode150_1/Q_86_Minimum_Absolute_Difference_in_BST.java
@@ -2,6 +2,38 @@ package LeetCode150.LeetCode150_1;
 
 public class Q_86_Minimum_Absolute_Difference_in_BST {
     public static void main(String[] args) {
-        
+        // Build BST: [4,2,6,1,3]
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        root.right = new TreeNode(6);
+
+        System.out.println("Input Tree : [4, 2, 6, 1, 3]");
+        System.out.println("Minimum Absolute Difference: " + getMinimumDifference(root)); 
+    }
+    public static int minDiff=Integer.MAX_VALUE;
+    public static TreeNode prev=null;
+
+    public static int getMinimumDifference(TreeNode root){
+        minDiff=Integer.MAX_VALUE;
+        prev=null;
+        inorder(root);
+        return minDiff;
+    }
+    public static void inorder(TreeNode node){
+        if(node==null){ return;}
+        inorder(node.left);
+        if(prev!=null){
+            int newDiff=node.val-prev.val;
+            minDiff=Math.min(minDiff, newDiff);
+        }
+        prev=node;
+        inorder(node.right);
+    }
+     public static class TreeNode{
+        int val;TreeNode left;TreeNode right;
+        TreeNode(){};        TreeNode(int val){this.val=val;}
+        TreeNode(int val,TreeNode left,TreeNode right){
+            this.val=val;this.left=left;this.right=right;
+        };
     }
 }
