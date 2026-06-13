e5d82c991060ca8ac8e979d5bfcb8d7c3a52ad23
tomar <tomarankit78@gmail.com>
2026-06-13 19:03:38 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_94_Course_Schedule_II.java
+++ LeetCode150/LeetCode150_1/Q_94_Course_Schedule_II.java
@@ -1,16 +1,26 @@
 package LeetCode150.LeetCode150_1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class Q_94_Course_Schedule_II {
     public static void main(String[] args) {
-        
+        int numCourses1 = 2;        int[][] prerequisites1 = {{1, 0}};
+        int numCourses2 = 4;        int[][] prerequisites2 = {{1,0},{2,0},{3,1},{3,2}};
+
+        System.out.println("numCourses   : " + numCourses1);
+        System.out.println("Prerequisites: " + Arrays.deepToString(prerequisites1));
+        System.out.println("Order        : " + Arrays.toString(findOrder(numCourses1, prerequisites1)));
+
+        System.out.println("\nnumCourses   : " + numCourses2);
+        System.out.println("Prerequisites: " + Arrays.deepToString(prerequisites2));
+        System.out.println("Order        : " + Arrays.toString(findOrder(numCourses2, prerequisites2)));
     }
     public static int[] findOrder(int numCourses,int[][] prerequisites){
-        List<List>adj=new ArrayList<>();
+        List<List<Integer>>adj=new ArrayList<>();
         int[] inDegree=new int[numCourses];
 
         for(int i=0;i<numCourses;i++){
