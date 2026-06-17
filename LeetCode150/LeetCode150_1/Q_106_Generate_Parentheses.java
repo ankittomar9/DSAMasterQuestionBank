5dfc1dc55bd7f58ccb9caab74f564b349ad0178d
tomar <tomarankit78@gmail.com>
2026-06-17 14:02:25 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_106_Generate_Parentheses.java
+++ LeetCode150/LeetCode150_1/Q_106_Generate_Parentheses.java
@@ -1,5 +1,34 @@
 package LeetCode150.LeetCode150_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q_106_Generate_Parentheses {
-    
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(3));  
+    }
+    public static List<String> generateParenthesis(int n){
+        List<String> result=new ArrayList<>();
+        backtrack(result,new StringBuilder(),0,0,n);
+        return result;
+    }
+    public static void backtrack(List<String> result,StringBuilder current,
+        int open,int close,int max){
+            if(current.length()==max*2){
+                result.add(current.toString());
+                return;
+            }
+
+            if(open < max){
+                current.append('(');
+                backtrack(result, current, open+1, close, max);
+                current.deleteCharAt(current.length()-1);
+            }
+
+            if(close<open){
+                current.append(')');
+                backtrack(result, current, open, close+1, max);
+                current.deleteCharAt(current.length()-1);
+            }
+        }
 }
