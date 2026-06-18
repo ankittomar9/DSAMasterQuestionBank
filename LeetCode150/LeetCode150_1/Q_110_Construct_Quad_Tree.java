b8a9b824c15ee3cebbb1ac3057e7bf6b4acd9f5f
tomar <tomarankit78@gmail.com>
2026-06-18 23:34:03 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_110_Construct_Quad_Tree.java
+++ LeetCode150/LeetCode150_1/Q_110_Construct_Quad_Tree.java
@@ -1,7 +1,54 @@
 package LeetCode150.LeetCode150_1;
-
 public class Q_110_Construct_Quad_Tree {
     public static void main(String[] args) {
-        
+        int[][] grid = {{0,1},{1,0}};
+        Node root = construct(grid);
+
+        System.out.println("Input Grid: [[0,1],[1,0]]");
+        System.out.println("Quad Tree constructed successfully.");
+        System.out.println("Root isLeaf: " + root.isLeaf);
+
+    }
+    public static Node construct(int[][] grid){
+        return construct(grid,0, 0, grid.length);
+    }
+    public static Node construct(int[][] grid,int row,int col,int size){
+        if(size==1){
+            return new Node(grid[row][col]==1 ,true);
+        }
+        int newSize=size/2;
+
+        Node topLeft=construct(grid, row, col, newSize);
+        Node topRight=construct(grid, row, col+newSize, newSize);
+        Node bottomLeft=construct(grid, row+newSize, col, newSize);
+          Node bottomRight = construct(grid, row + newSize, col + newSize, newSize);
+
+        if(
+         topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf &&
+         topLeft.val==topRight.val && topRight.val ==bottomLeft.val && bottomLeft.val==bottomRight.val){
+            return new Node(topLeft.val,true);
+
+         }
+         return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
+
+    }
+    public static class Node{
+        public boolean val;        public boolean isLeaf;        public Node topLeft;
+        public Node topRight;        public Node bottomLeft;        public Node bottomRight;
+
+           public Node(boolean val, boolean isLeaf) {
+            this.val = val;
+            this.isLeaf = isLeaf;
+        }
+
+        public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+            this.val = val;
+            this.isLeaf = isLeaf;
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+            this.bottomLeft = bottomLeft;
+            this.bottomRight = bottomRight;
+        }
+
     }
 }
