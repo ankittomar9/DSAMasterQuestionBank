9d5820da95c336a3d41bf8d9f754c64056b8977a
tomar <tomarankit78@gmail.com>
2026-06-21 20:52:07 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_115_Search_a_2D_Matrix.java
+++ LeetCode150/LeetCode150_1/Q_115_Search_a_2D_Matrix.java
@@ -1,5 +1,40 @@
 package LeetCode150.LeetCode150_1;
 
 public class Q_115_Search_a_2D_Matrix {
-    
+ public static void main(String[] args) {
+      int[][] matrix = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+        System.out.println("Input Matrix:");
+        for (int[] row : matrix) {
+            for (int val : row) System.out.print(val + "  ");
+            System.out.println();
+        }
+        System.out.println("\nTarget = 3  → " + searchMatrix(matrix, 3));
+        System.out.println("Target = 13 → " + searchMatrix(matrix, 13));
+ }   
+ public static boolean searchMatrix(int[][] matrix,int target){
+    if(matrix==null ||matrix.length==0 || matrix[0].length==0){
+        return false;
+    }
+    int m=matrix.length; int n=matrix[0].length;
+
+    int left=0; int right=m*n-1;
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        int row=mid/n;
+        int col=mid%n;
+
+        if(matrix[row][col] ==target){
+            return true;
+        }else if(matrix[row][col] < target){
+            left=mid+1; 
+        }else{
+            right=mid-1;
+        }
+    }
+    return false;
+ }
 }
