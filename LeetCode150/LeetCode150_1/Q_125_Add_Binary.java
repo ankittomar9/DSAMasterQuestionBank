990b7f454b35040797b4541fa94e76196f227487
tomar <tomarankit78@gmail.com>
2026-06-24 13:48:14 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_125_Add_Binary.java
+++ LeetCode150/LeetCode150_1/Q_125_Add_Binary.java
@@ -5,4 +5,27 @@ public class Q_125_Add_Binary {
         System.out.println(addBinary("11", "1"));     // "100"
         System.out.println(addBinary("1010", "1011")); // "10101"
     }
+    public static String addBinary(String a ,String b){
+        StringBuilder sb=new StringBuilder();
+        int i=a.length()-1;int j=b.length()-1;
+        int carry=0;
+
+
+        while(i>=0 || j>=0 || carry>0){
+            int sum=carry;
+            if(i>=0){
+                sum=sum+a.charAt(i)-'0';
+                i--;
+            }
+
+            if(j>=0){
+                sum=sum+b.charAt(j)-'0';
+                j--;
+            }
+            int newSum=sum%2;
+            sb.append(newSum);
+            carry=sum/2;
+        }
+        return sb.reverse().toString();
+    }
 }
