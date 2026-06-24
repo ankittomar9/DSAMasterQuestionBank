3b2981c70945bb56f02628176b567e8fa079e99e
tomar <tomarankit78@gmail.com>
2026-06-24 13:02:01 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_123_Find_K_Pairs_with_Smallest_Sums.java
+++ LeetCode150/LeetCode150_1/Q_123_Find_K_Pairs_with_Smallest_Sums.java
@@ -1,7 +1,11 @@
 package LeetCode150.LeetCode150_1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public class Q_123_Find_K_Pairs_with_Smallest_Sums {
     public static void main(String[] args) {
@@ -16,6 +20,33 @@ public class Q_123_Find_K_Pairs_with_Smallest_Sums {
         System.out.println("K Smallest Pairs: " + result);
     }
     public static List<List<Integer>>kSmallestPairs(int[] nums1,int[] nums2,int k){
+        List<List<Integer>> result=new ArrayList<>();
+        if(nums1.length==0 || nums2.length==0 || k==0) return result;
 
+        PriorityQueue<int[]> minHeap=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        Set<String> visited=new HashSet<>();
+
+        minHeap.offer(new int[]{nums1[0]+nums2[0],0,0});
+        visited.add("0,0");
+
+        while(k>0 && !minHeap.isEmpty()){
+            int[] curr=minHeap.poll();
+            int sum=curr[0]; int i=curr[1];int j=curr[2];
+
+            result.add(Arrays.asList(nums1[i],nums2[j]));
+            k--;
+
+            if(j+1 <nums2.length && !visited.contains(i+","+(j+1))){
+                minHeap.offer(new int[]{nums1[i]+nums2[j+1],i,j+1});
+                visited.add(i+","+(j+1));
+            }
+
+            
+            if(i+1 <nums1.length && !visited.contains((i+1)+","+(j))){
+                minHeap.offer(new int[]{nums1[i+1]+nums2[j],i+1,j});
+                visited.add((i+1)+","+(j));
+            }
+        }
+        return result;
     }
 }
