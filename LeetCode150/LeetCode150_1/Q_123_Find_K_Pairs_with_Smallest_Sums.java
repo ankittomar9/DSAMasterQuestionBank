91db37549da73aff5a4225913e7054bee98f496b
tomar <tomarankit78@gmail.com>
2026-06-25 12:47:40 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_123_Find_K_Pairs_with_Smallest_Sums.java
+++ LeetCode150/LeetCode150_1/Q_123_Find_K_Pairs_with_Smallest_Sums.java
@@ -50,3 +50,31 @@ public class Q_123_Find_K_Pairs_with_Smallest_Sums {
         return result;
     }
 }
+
+/*
+import java.util.*;
+class Solution {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> res=new ArrayList<>();
+
+        if(nums1.length==0 || nums2.length==0 || k==0)return res;
+        PriorityQueue<int[]> minHeap=new PriorityQueue<>(
+            (a,b)-> (nums1[a[0]]+nums2[a[1]])-(nums1[b[0]]+nums2[b[1]])
+        );
+
+        for(int i=0;i<Math.min(nums1.length,k);i++)minHeap.offer(new int[]{i,0});
+        while (k>0 && !minHeap.isEmpty()){
+            int[] curr=minHeap.poll();
+            int i=curr[0];
+            int j=curr[1];
+            res.add(Arrays.asList(nums1[i],nums2[j]));
+            k--;
+
+            if(j+1<nums2.length)minHeap.offer(new int[]{i,j+1});
+        }
+        return res;
+    }
+}
+
+
+*/
