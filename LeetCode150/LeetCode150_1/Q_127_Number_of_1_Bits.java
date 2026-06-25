074bd05560309d8b04cd587c1e0f318a5943cf62
tomar <tomarankit78@gmail.com>
2026-06-25 12:47:48 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_127_Number_of_1_Bits.java
+++ LeetCode150/LeetCode150_1/Q_127_Number_of_1_Bits.java
@@ -7,4 +7,12 @@ public class Q_127_Number_of_1_Bits {
             String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0'));
         System.out.println("Number of 1 Bits: " + hammingWeight(n));
     }
+    public static int hammingWeight(int n){
+        int count=0;
+        while(n!=0){
+            n&=(n-1);
+            count++;
+        }
+        return count;
+    }
 }
