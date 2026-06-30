2aea945c07f36ce4da1041b65a1e7709407e64d0
tomar <tomarankit78@gmail.com>
2026-06-30 23:54:07 +0530

    New Added Program DSA

--- LeetCode150/LeetCode150_1/Q_148_Best_Time_to_Buy_and_Sell_Stock_III.java
+++ LeetCode150/LeetCode150_1/Q_148_Best_Time_to_Buy_and_Sell_Stock_III.java
@@ -8,4 +8,20 @@ public class Q_148_Best_Time_to_Buy_and_Sell_Stock_III {
         System.out.println("prices = " + Arrays.toString(prices));
         System.out.println("Maximum Profit with at most 2 transactions: " + maxProfit(prices));
     }
+    public static int maxProfit(int[] prices){
+        int buy1=Integer.MIN_VALUE;int buy2=Integer.MIN_VALUE;
+        int sell1=0;              int sell2=0;
+    
+        for(int i=0;i<prices.length;i++){
+
+            buy1=Math.max(buy1,-prices[i]);
+
+            sell1=Math.max(sell1, buy1+prices[i]);
+
+            buy2=Math.max(buy2, sell1-prices[i]);
+            
+            sell2=Math.max(sell2, buy2+prices[i]);
+        }
+        return sell2;
+    }
 }
