bf49c46b1592b2fd9e007025352aedcfe5df5bef
tomar <tomarankit78@gmail.com>
2026-07-06 22:48:13 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_6_3Sum_Closest.java
+++ LeetCodeAll/arrays/Q_6_3Sum_Closest.java
@@ -13,8 +13,7 @@ public class Q_6_3Sum_Closest {
     public static int threeSumClosest(int[] nums,int target){
         if(nums==null || nums.length==0){ return 0;}
     
-        Arrays.sort(nums);
-        int n=nums.length;
+        Arrays.sort(nums);        int n=nums.length;
         int closestSum=nums[0]+nums[1]+nums[2];
 
         for(int i=0;i<n-2;i++){
