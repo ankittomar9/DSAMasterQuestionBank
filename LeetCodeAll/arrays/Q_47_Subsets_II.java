ff1e62b7a5e8b767bd84d09a78721bbf635aaec6
tomar <tomarankit78@gmail.com>
2026-07-08 15:25:31 +0530

    New Added Program DSA

---
+++ LeetCodeAll/arrays/Q_47_Subsets_II.java
@@ -0,0 +1,38 @@
+package LeetCodeAll.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q_47_Subsets_II {
+    public static void main(String[] args) {
+         int[] nums1 = {1, 2, 2};
+        System.out.println("nums = " + Arrays.toString(nums1));
+        List<List<Integer>> result1 = subsetsWithDup(nums1);
+        System.out.println("All unique subsets (" + result1.size() + " total):");
+        for (List<Integer> subset : result1) System.out.println(subset);
+        System.out.println();
+        int[] nums3 = {2, 2, 2};
+        System.out.println("nums = " + Arrays.toString(nums3));
+        List<List<Integer>> result3 = subsetsWithDup(nums3);
+        System.out.println("All unique subsets (" + result3.size() + " total):");
+        for (List<Integer> subset : result3) System.out.println(subset);
+    }
+      public static List<List<Integer>> subsetsWithDup(int[] nums){
+        List<List<Integer>> result=new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums,0,new ArrayList<>(),result);
+        return result;
+    }
+    public static void backtrack(int[] nums,int start,
+        List<Integer> current,List<List<Integer>>result){
+        result.add(new ArrayList<>(current));
+
+        for(int i=start;i<nums.length;i++){
+            if(i>start && nums[i]==nums[i-1]) continue;
+            current.add(nums[i]);
+            backtrack(nums, i+1, current, result);
+            current.remove(current.size()-1);
+        }
+    }
+}
