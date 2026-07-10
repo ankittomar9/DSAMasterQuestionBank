4fd9716d7fb670ea0329269f347c691af74ecffa
tomar <tomarankit78@gmail.com>
2026-07-10 23:44:31 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_72_Maximum_Gap.java
+++ LeetCodeAll/arrays/Q_72_Maximum_Gap.java
@@ -1,5 +1,15 @@
 package LeetCodeAll.arrays;
 
+import java.util.Arrays;
+
 public class Q_72_Maximum_Gap {
+    public static void main(String[] args) {
+        int[] nums1 = {3, 6, 9, 1};        System.out.println("nums = " + Arrays.toString(nums1));
+        System.out.println("Maximum Gap: " + maximumGap(nums1));
+         System.out.println();        int[] nums3 = {1, 1, 1, 1};        System.out.println("nums = " + Arrays.toString(nums3));
+        System.out.println("Maximum Gap: " + maximumGap(nums3));        System.out.println();
+        int[] nums5 = {100, 3, 2, 1, 4, 5, 6, 7};        System.out.println("nums = " + Arrays.toString(nums5));
+        System.out.println("Maximum Gap: " + maximumGap(nums5));
+    }
     
 }
