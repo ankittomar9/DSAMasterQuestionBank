38fd5e47da987cb2792b2a505b7ba2aac0a08113
tomar <tomarankit78@gmail.com>
2026-07-10 23:45:43 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_76_Dungeon_Game.java
+++ LeetCodeAll/arrays/Q_76_Dungeon_Game.java
@@ -2,6 +2,10 @@ package LeetCodeAll.arrays;
 
 public class Q_76_Dungeon_Game {
     public static void main(String[] args) {
+          int[][] dungeon1 = {{-2, -3, 3}, {-5, -10, 1}, {10, 30, -5}};
+        System.out.println("dungeon = [[-2,-3,3],[-5,-10,1],[10,30,-5]]");
+        System.out.println("Minimum Initial Health: " + calculateMinimumHP(dungeon1));
+
         
     }
 }
