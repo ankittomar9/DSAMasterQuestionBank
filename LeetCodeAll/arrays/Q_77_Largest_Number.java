837e514cff2bdd2ecdaa4a2dfa641660c9d8bc04
tomar <tomarankit78@gmail.com>
2026-07-10 23:45:45 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_77_Largest_Number.java
+++ LeetCodeAll/arrays/Q_77_Largest_Number.java
@@ -1,5 +1,23 @@
 package LeetCodeAll.arrays;
 
+import java.util.Arrays;
+
 public class Q_77_Largest_Number {
-    
+    public static void main(String[] args) {
+        int[] nums1 = {10, 2};
+        System.out.println("nums = " + Arrays.toString(nums1));
+        System.out.println("Largest Number: " + largestNumber(nums1));
+
+        // Test 2
+        System.out.println();
+        int[] nums2 = {3, 30, 34, 5, 9};
+        System.out.println("nums = " + Arrays.toString(nums2));
+        System.out.println("Largest Number: " + largestNumber(nums2));
+
+        // Test 3: all zeros
+        System.out.println();
+        int[] nums3 = {0, 0};
+        System.out.println("nums = " + Arrays.toString(nums3));
+        System.out.println("Largest Number: " + largestNumber(nums3));
+    }
 }
