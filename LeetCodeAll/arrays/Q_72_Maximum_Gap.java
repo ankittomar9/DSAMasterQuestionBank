47fc2d149c79861240b566d6b70dfa8e040f8e93
tomar <tomarankit78@gmail.com>
2026-07-11 23:49:37 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_72_Maximum_Gap.java
+++ LeetCodeAll/arrays/Q_72_Maximum_Gap.java
@@ -12,6 +12,32 @@ public class Q_72_Maximum_Gap {
         System.out.println("Maximum Gap: " + maximumGap(nums5));
     }
     public static int maximumGap(int[] nums){
-        
+        int n=nums.length;        if(n<2){return 0;}
+        int globalMin=nums[0],globalMax=nums[0];
+        for(int i=0;i<nums.length;i++){ globalMin=Math.min(globalMin, nums[i]); globalMax=Math.max(globalMax,nums[i]);
+                }
+                if(globalMin==globalMax) return 0;
+                int newMax=(globalMax-globalMin)/(n-1);
+                int bucketSize=Math.max(1, newMax);
+    
+           int numBuckets=(globalMax-globalMin)/bucketSize+1;
+           
+           int[] bucketMin=new int[numBuckets];           int[] bucketMax=new int[numBuckets];
+           Arrays.fill(bucketMax, Integer.MIN_VALUE); Arrays.fill(bucketMin, Integer.MAX_VALUE);
+
+           for(int i=0;i<nums.length;i++){
+            int bucketIdx=(nums[i]-globalMin)/bucketSize;
+             bucketMin[bucketIdx]=Math.min(bucketMin[bucketIdx], nums[i]);
+            bucketMax[bucketIdx]=Math.max(bucketMax[bucketIdx], nums[i]);
+           }
+
+           int maxGap=0; int prevMax=globalMin;
+
+           for(int i=0;i<numBuckets;i++){
+            if(bucketMin[i] ==Integer.MAX_VALUE) continue;
+            maxGap=Math.max(maxGap, bucketMin[i]-prevMax);
+            prevMax=bucketMax[i];
+           }
+           return maxGap;
     }
 }
