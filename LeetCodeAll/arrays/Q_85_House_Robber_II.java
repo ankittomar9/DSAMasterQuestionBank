5282c210b09212f5c5ca3960b36228a6b3309310
tomar <tomarankit78@gmail.com>
2026-07-14 22:32:42 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_85_House_Robber_II.java
+++ LeetCodeAll/arrays/Q_85_House_Robber_II.java
@@ -21,5 +21,27 @@ public class Q_85_House_Robber_II {
         System.out.println("nums = " + Arrays.toString(nums3));
         System.out.println("Max Robbery: " + rob(nums3));
     }
-    
+    public static int rob(int[] nums){
+        int n=nums.length; 
+        if(n==1){return nums[0];}
+        if(n==2){return Math.max(nums[0], nums[1]);}
+
+        int caseA=robLinear(nums,0,n-2);
+        int caseB=robLinear(nums,1,n-1);
+        return Math.max(caseA, caseB);
+    }
+
+    public static int robLinear(int[] nums,int start,int end){
+        int prev2=0;
+        int prev1=0;
+
+        for(int i=start;i<=end;i++){
+            int curr=Math.max(prev1, prev2+nums[i]);
+
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+
 }
