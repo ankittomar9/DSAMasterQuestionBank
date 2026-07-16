4238e007c1e7048f01710adef3d61fd52f46c818
tomar <tomarankit78@gmail.com>
2026-07-16 08:08:06 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_91_Contains_Duplicate_III.java
+++ LeetCodeAll/arrays/Q_91_Contains_Duplicate_III.java
@@ -1,5 +1,9 @@
 package LeetCodeAll.arrays;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Q_91_Contains_Duplicate_III {
     public static void main(String[] args) {
        int[] nums1 = {1, 2, 3, 1};
@@ -19,6 +23,36 @@ public class Q_91_Contains_Duplicate_III {
         System.out.println("Result: " + containsNearbyAlmostDuplicate(nums3, 1, 2));  
     }
     public static boolean containsNearbyAlmostDuplicate(int[] nums,int indexDiff,int valueDiff){
+        int n=nums.length;
+        long bucketWidth=(long) valueDiff+1;
 
+       Map<Long, Long> bucketMap=new HashMap<>();
+        for (int i = 0; i < n; i++) {
+        long currentVal = nums[i];
+        long bucketId = getBucketId(currentVal, bucketWidth);
+            if (bucketMap.containsKey(bucketId)) return true;
+
+              Long leftNeighbor = bucketMap.get(bucketId - 1);
+        if (leftNeighbor!=null && Math.abs(currentVal - leftNeighbor)<=valueDiff) return true;
+
+          Long rightNeighbor=bucketMap.get(bucketId + 1);
+        if (rightNeighbor!=null && Math.abs(currentVal - rightNeighbor)<=valueDiff) return true;
+
+          bucketMap.put(bucketId, currentVal);
+
+           if (i >= indexDiff) {
+            long evictVal = nums[i - indexDiff];
+            long evictBucketId = getBucketId(evictVal, bucketWidth);
+            bucketMap.remove(evictBucketId);
+        }
     }
+    return false;
+}     
+    private static long getBucketId(long value, long width) {
+    if (value >= 0) {
+        return value / width;
+    } else {
+        return (value + 1) / width - 1;
+    }
+}
 }
