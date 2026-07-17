0ec2605346a71ceb60985ed5c6aec5967e98d989
tomar <tomarankit78@gmail.com>
2026-07-17 06:30:32 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_99_Single_Number_III.java
+++ LeetCodeAll/arrays/Q_99_Single_Number_III.java
@@ -1,5 +1,25 @@
 package LeetCodeAll.arrays;
 
+import java.util.Arrays;
+
 public class Q_99_Single_Number_III {
+    public static void main(String[] args) {
+         // Test 1
+        int[] nums1 = {1, 2, 1, 3, 2, 5};
+        System.out.println("nums = " + Arrays.toString(nums1));
+        System.out.println("Result: " + Arrays.toString(singleNumber(nums1)));
+
+        // Test 2: negative numbers
+        System.out.println();
+        int[] nums2 = {-1, 0};
+        System.out.println("nums = " + Arrays.toString(nums2));
+        System.out.println("Result: " + Arrays.toString(singleNumber(nums2)));
+              System.out.println();
+        int[] nums4 = {1, 1, 2, 2, 3, 4, 4, 5};
+        System.out.println("nums = " + Arrays.toString(nums4));
+        System.out.println("Result: " + Arrays.toString(singleNumber(nums4)));
+
+    }
     
+
 }
