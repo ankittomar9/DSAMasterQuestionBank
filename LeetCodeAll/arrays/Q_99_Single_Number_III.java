0fb17eb07d19920e6ad4db5724c419759434c46c
tomar <tomarankit78@gmail.com>
2026-07-17 06:35:53 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_99_Single_Number_III.java
+++ LeetCodeAll/arrays/Q_99_Single_Number_III.java
@@ -20,6 +20,26 @@ public class Q_99_Single_Number_III {
         System.out.println("Result: " + Arrays.toString(singleNumber(nums4)));
 
     }
-    
+    public static int[] singleNumber(int[] nums){
+        int xorAll=0;
+
+        for(int i=0;i<nums.length;i++){
+            xorAll=xorAll^nums[i];
+        }
+
+        int diffBit=xorAll & (-xorAll);
+
+        int group1=0;
+        int group2=0;
+
+        for(int i=0;i<nums.length;i++){
+            if((nums[i] & diffBit) !=0){
+                group1=group1^nums[i];
+            }else{
+                group2=group2^nums[i];
+            }
+        }
+        return new int[]{group1,group2};
+    }
 
 }
