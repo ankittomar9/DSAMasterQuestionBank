28dc76c0ec715d9c5eafe2ebf663c6098ee00f92
tomar <tomarankit78@gmail.com>
2026-08-02 22:10:36 +0530

    New Added Program DSA

---
+++ LeetCodeAll/arrays/Q_113_Super_Ugly_Number_Heap.java
@@ -0,0 +1,13 @@
+package LeetCodeAll.arrays;
+
+import java.util.Arrays;
+
+public class Q_113_Super_Ugly_Number_Heap {
+    public static void main(String[] args) {
+            int[] primes1 = {2, 7, 13, 19};
+        System.out.println("n=12, primes=" + Arrays.toString(primes1));
+        System.out.println("Result: " + nthSuperUglyNumber(12, primes1));
+    }
+    
+
+}
