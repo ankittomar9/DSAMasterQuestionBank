6de2bef25bb47c55722120dd0643dfcc2b00c0b1
tomar <tomarankit78@gmail.com>
2026-08-04 16:30:01 +0530

    New Added Program DSA

---
+++ LeetCodeAll/arrays/Q_31_Spiral_Matrix_II.java
@@ -0,0 +1,12 @@
+package LeetCodeAll.arrays;
+
+public class Q_31_Spiral_Matrix_II {
+    public static void main(String[] args) {
+         System.out.println("n = 3");        printMatrix(generateMatrix(3));
+
+        System.out.println("\nn = 4");        printMatrix(generateMatrix(4));
+
+        System.out.println("\nn = 1");        printMatrix(generateMatrix(1));
+    }
+
+}
