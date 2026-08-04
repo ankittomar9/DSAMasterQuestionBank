8bbfb3f083bb919c51a3d7da994492d245e97703
tomar <tomarankit78@gmail.com>
2026-08-04 16:07:15 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_15_Sudoku_Solver.java
+++ LeetCodeAll/arrays/Q_15_Sudoku_Solver.java
@@ -21,18 +21,55 @@ public class Q_15_Sudoku_Solver {
         solveSudoku(board); System.out.println("\nSolved Board:"); printBoard(board);
     }
      public static void solveSudoku(char[][] board) {
-        solve(board);
-    }
-    public static boolean  solve(char[][] board){
+      
         boolean[][] rows=new boolean[9][9];
         boolean[][] cols=new boolean[9][9];
         boolean[][] boxes=new boolean[9][9];
-
         List<int[]> emptyCells=new ArrayList<>();
 
-        
+        for(int r=0;r<9;r++){
+            for(int c=0;c<9;c++){
+                if(board[r][c] !='.'){
+                    int d=board[r][c]-'1';
+                    int box=(r/3)*3+ (c/3);
+                    rows[r][d]=cols[c][d]=boxes[box][d]=true;
+                }else{
+                    emptyCells.add(new int[]{r,c});
+                }
+            }
+        }
+    backtrack(board, emptyCells, 0, rows, cols, boxes) ;
     }
- public static boolean isValid(char[][] board, int row, int col, char digit) {
-    
- }
+      private static boolean backtrack(char[][] board, List<int[]> emptyCells, int index,
+                                     boolean[][] rows, boolean[][] cols, boolean[][] boxes) {
+            if (index == emptyCells.size()) return true;
+                        
+            int r=emptyCells.get(index)[0];
+            int c=emptyCells.get(index)[1];
+            int box=(r/3)*3+ (c/3); 
+            
+            for(int d=0;d<9;d++){
+                if(!rows[r][d] && !cols[c][d] && !boxes[box][d]){
+                    board[r][c]=(char)('1'+d);
+                    rows[r][d]=cols[c][d]=boxes[box][d]=true;
+
+                    if(backtrack(board, emptyCells, index+1, rows, cols, boxes)){
+                        return true;
+                    }
+                    board[r][c]='.';
+                    rows[r][d]=cols[c][d]=boxes[box][d]=false;
+                }
+            }
+            return false;
+    }
+   private static void printBoard(char[][] board) {
+        for (int r = 0; r < 9; r++) {
+            if (r == 3 || r == 6) System.out.println("------+-------+------");
+            for (int c = 0; c < 9; c++) {
+                if (c == 3 || c == 6) System.out.print("| ");
+                System.out.print(board[r][c] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
