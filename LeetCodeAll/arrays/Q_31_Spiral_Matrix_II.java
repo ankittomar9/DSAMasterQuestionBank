9520492402d5d41102b98b758276e722aaa98642
tomar <tomarankit78@gmail.com>
2026-08-05 20:00:33 +0530

    New Added Program DSA

--- LeetCodeAll/arrays/Q_31_Spiral_Matrix_II.java
+++ LeetCodeAll/arrays/Q_31_Spiral_Matrix_II.java
@@ -8,5 +8,33 @@ public class Q_31_Spiral_Matrix_II {
 
         System.out.println("\nn = 1");        printMatrix(generateMatrix(1));
     }
+    public static int[][] generateMatrix(int n){
+        int[][] matrix=new int[n][n];
+        int[] dr={0,1,0,-1};
+        int[] dc={1,0,-1,0};
+        int dir=0,row=0,col=0;
+        for(int num=1;num<=n*n ;num++){
+            matrix[row][col]=num;
+            int nextRow = row+dr[dir];
+            int nextCol = col+dc[dir];
 
+            if(nextRow<0 || nextRow>=n || nextCol<0 || nextCol >=n || matrix[nextRow][nextCol] !=0){
+                dir=(dir+1) %4;
+            }
+            row=row+dr[dir];
+            col=col+dc[dir];
+        }
+        return matrix;
+    }
+
+     private static void printMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int width = String.valueOf(n * n).length() + 1;
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.printf("%" + width + "d", val);
+            }
+            System.out.println();
+        }
+    }
 }
